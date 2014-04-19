2f0d154314ad28560a84fe7bff7c5d1c671993cf
Thomas Bopst <thomas@bopst.de>
2014-04-19 14:26:08 +0200

    Fixes ajax status code check.

--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -7,7 +7,7 @@ function sendGuessHelper() {
 function sendGuess(g) {
   var requestObj = new XMLHttpRequest();
   requestObj.onreadystatechange = function() {
-    if (requestObj.readyState == 4 || requestObj.readyState == 200){
+    if (requestObj.readyState == 4 && requestObj.status == 200){
       document.getElementById("output").innerHTML = requestObj.responseText;
       requestObj = null;
     }
